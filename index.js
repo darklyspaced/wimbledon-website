8eec691edea86cc144e7e351d1335d79998dcb36
darklyspaced <srohanjd@gmail.com>
2022-11-14 20:38:04 +0800

    start of animation on restuarants page

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const hiddenElements = document.querySelectorAll('.hidden');
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        console.log(entry);
-        if(entry.intersectionRatio > 0) {
-            console.log("tesafkdjsalkfjdsal;kjfkds:w")
-            entry.classList.add('show');
-        }
-    })
-});
-
-hiddenElements.forEach(el => {
-    observer.observe(el);
-})
+// const hiddenElements = document.querySelectorAll('.hidden');
+//
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach(entry => {
+//         console.log(entry);
+//         if(entry.intersectionRatio > 0) {
+//             console.log("tesafkdjsalkfjdsal;kjfkds:w")
+//             entry.classList.add('show');
+//         }
+//     })
+// });
+//
+// hiddenElements.forEach(el => {
+//     observer.observe(el);
+// })
+const gallery_items = document.querySelectorAll('.gallery_item');
+console.log(gallery_items);
