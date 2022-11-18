bd1b58ea0c07720745fb66963e9a8b4972b48bba
darklyspaced <srohanjd@gmail.com>
2022-11-18 15:54:24 +0800

    inclusion of scroll-blur v1

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // have functions that add classes that perform animations
+import("jquery");
 
 const gallery_items = document.getElementsByClassName('gallery_item');
-console.log(gallery_items.length);
+console.log(gallery_items);
+
 for (let i = 0; i < gallery_items.length; i++) {
     gallery_items[i].addEventListener("click", expand);
-    console.log("test");
 }
 
-function expand(){
-    console.log("test");
-};
+$(window).on('scroll', function() {
+    var s = $(window).scrollTop; //set current position to the top
+    opacityVal = (s / 240); //denominator changes how fast it blurs || bigger is slower blur
+    $('.blur').css('opacity', opacityVal);
+}); 
