4613b8bfdf411525c9c25caf80e8e2af9c6c4d86
darklyspaced <srohanjd@gmail.com>
2022-12-01 21:18:52 +0800

    working version

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ console.log(gallery_items);
 for (let i = 0; i < gallery_items.length; i++) {
     gallery_items[i].addEventListener("click", expand);
 }
+
+var lastScrollTop = $(window).scrollTop();
+
+$(window).on('scroll', function() { 
+    var s = $(this).scrollTop();
+    var change = s = lastScrollTop;
+    opacityVal = (s / 240);
+    $('.titles').css({
+        bottom: “-=” + change/4.5,
+        opacity: “-=” + change/700
+    });
+    lastScrollTop = s;
+});
