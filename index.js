d3ea2f55c3688c89711c3728f48b57c411baa422
darklyspaced <srohanjd@gmail.com>
2022-12-01 20:22:54 +0800

    added homepage blocks

--- index.js
+++ index.js
@@ -7,9 +7,3 @@ console.log(gallery_items);
 for (let i = 0; i < gallery_items.length; i++) {
     gallery_items[i].addEventListener("click", expand);
 }
-
-$(window).on('scroll', function() {
-    var s = $(window).scrollTop; //set current position to the top
-    opacityVal = (s / 240); //denominator changes how fast it blurs || bigger is slower blur
-    $('.blur').css('opacity', opacityVal);
-}); 
